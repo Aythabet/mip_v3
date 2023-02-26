ab288ba9498f2ba50712dbd15cd5d4809d4737d6
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-02-26 18:12:25 +0100

    Resolved webpack problem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,12 +6,15 @@ class TasksController < ApplicationController
 
   def retrieve_tasks
     TasksJob.perform_async
+
     flash.notice = 'The import started, come back in few minutes!'
     redirect_to tasks_path
   end
 
   def destroy_all
-    Task.destroy_all
+    DestroyTasksJob.perform_async
+
+    flash.notice = 'All the tasks will be deleted.'
     redirect_to tasks_path
   end
 end
