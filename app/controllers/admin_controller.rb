fb23573d99f94b546690621230c5b1c9598fd70d
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-05-01 08:20:34 +0100

    Added half days vacations to active lists
    Query modification in the controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,10 @@ class AdminController < ApplicationController
   before_action :check_admin
 
   def all_vacations
-    @active_vacations = Vacation.where("start_date <= ? AND ? <= (start_date + interval '1 day' * (duration - 1))", Date.today, Date.today)
+    @active_vacations = Vacation.where(
+      "start_date <= ? AND ? <= (start_date + interval '1 day' * ((duration - 0.5) * 2))",
+      Date.today,
+      Date.today
+    )
   end
 end
