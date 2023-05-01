35c9486e84313a0f184ec1b784970d22b7dbf660
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-05-01 15:35:18 +0100

    More updates on the vacation lists shown to the admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,5 +7,14 @@ class AdminController < ApplicationController
       Date.today,
       Date.today
     )
+    @upcoming_vacations = Vacation.where(
+      "start_date >= ? AND ? <= (start_date + interval '1 day' * ((duration - 0.5) * 2))",
+      Date.today,
+      Date.today
+    ).where.not(
+      "start_date <= ? AND ? <= (start_date + interval '1 day' * ((duration - 0.5) * 2))",
+      Date.today,
+      Date.today
+    )
   end
 end
