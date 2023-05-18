fc07804a622aed1fb467179a685ea9b5e9e2b12d
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-05-18 07:27:25 +0100

    Search on Tasks
    Search using range of dates on the last_jira_update field
    search using assignees

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,26 @@ class TasksController < ApplicationController
   def index
     breadcrumbs.add "Tasks", tasks_path
 
-    @tasks = Task.order(updated_at: :desc)
-    @tasks = @tasks.page params[:page]
+    @tasks = Task.order(updated_at: :desc).page(params[:page])
+
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @tasks = @tasks.where(last_jira_update: start_date..end_date)
+    end
+
+    if params[:assignee].present?
+      assignee_id = Assignee.find_by(name: params[:assignee]).id
+      @tasks = @tasks.where(assignee: assignee_id)
+    end
+
+    @tasks_count = Task.count
 
     if turbo_frame_request?
       render partial: "tasks", locals: { tasks: @tasks }
     else
       render :index
     end
-
-    @tasks_count = Task.count
   end
 
   def show
